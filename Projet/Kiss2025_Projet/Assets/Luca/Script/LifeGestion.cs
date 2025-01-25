c2067f7d72c6e329ce9d4ca34ee6003a59360b83
GeekLuc <luca.rougefort@student.heaj.be>
2025-01-25 19:04:43 +0100

    m

--- Projet/Kiss2025_Projet/Assets/Luca/Script/LifeGestion.cs
+++ Projet/Kiss2025_Projet/Assets/Luca/Script/LifeGestion.cs
@@ -8,24 +8,51 @@ public class LifeGestion : MonoBehaviour
     [SerializeField] public float LifeCurrent = 20.0f;
     [SerializeField] public float LifeMax = 20.0f;
     [SerializeField] private bool isPlayer = false;
+    [SerializeField] private AudioClip damageSound;
+    [SerializeField] private AudioClip healSound;
+    private AudioSource audioSource;
 
-    public void TakeDamage(float damage){
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource component is missing.");
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
         LifeCurrent -= damage;
+        if (audioSource != null && damageSound != null)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
         print(" DEGATS : Life: " + LifeCurrent);
-        if (LifeCurrent <= 0){
+        if (LifeCurrent <= 0)
+        {
             Destroy(gameObject);
-            if (isPlayer){
+            if (isPlayer)
+            {
                 SceneManager.LoadScene("S_GameOver");
-            }else if (gameObject.tag == "Ennemy"){
+            }
+            else if (gameObject.tag == "Ennemy")
+            {
                 VagueEnnemy vagueEnnemy = GameObject.Find("GameManager").GetComponent<VagueEnnemy>();
                 vagueEnnemy.DecrementEnemiesRemaining();
             }
         }
     }
 
-    public void TakeHealth(float health){
+    public void TakeHealth(float health)
+    {
         LifeCurrent += health;
-        if (LifeCurrent > LifeMax){
+        if (audioSource != null && healSound != null)
+        {
+            audioSource.PlayOneShot(healSound);
+        }
+        if (LifeCurrent > LifeMax)
+        {
             LifeCurrent = LifeMax;
         }
     }
